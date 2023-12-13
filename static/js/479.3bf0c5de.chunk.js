3c7160f5780d18e99bea2fc5d2ea249ead56c2fa
Bird-Forest <127691003+Bird-Forest@users.noreply.github.com>
2023-12-13 18:13:12 +0000

    Deploying to gh-pages from @ Bird-Forest/react-examles@ef658344e18201debef907b043957200288b6df7 🚀

--- static/js/479.3bf0c5de.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[479],{479:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});r(2791);var n=r(184);function u(){return(0,n.jsx)("div",{children:"BasketList"})}}}]);
-//# sourceMappingURL=479.3bf0c5de.chunk.js.map
